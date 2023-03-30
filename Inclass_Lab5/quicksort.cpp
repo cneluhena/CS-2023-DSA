979562da219a7884c35e4af20117a4aafdcea31e
cneluhena <129386035+cneluhena@users.noreply.github.com>
2023-03-30 16:37:44 +0530

    Update quicksort.cpp

--- Inclass_Lab5/quicksort.cpp
+++ Inclass_Lab5/quicksort.cpp
@@ -1,1 +1,45 @@
+#include <iostream>
+using namespace std;
 
+
+//partitioning the array
+int partition(int arr[], int start, int end){
+
+    int pivot = arr[end];
+    int i = start-1;
+    for (int j=start; j<= end -1; j++){
+        if (arr[j] < pivot){
+            i++;
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            
+        }
+    }
+    i++;
+    int temp = arr[i];
+    arr[i] = arr[end];
+    arr[end] = temp;
+            
+    return i;
+}
+
+void quicksort(int arr[], int start, int end){
+    if (end <= start)
+        return;
+    int pivot = partition(arr, start, end);
+    quicksort(arr, start, pivot-1);
+    quicksort(arr, pivot+1, end);
+    
+}
+
+
+int main(){
+    int array[] = {10, 2, 5, 3, 9, 4, 7, 6, 1};
+    int arr_length = sizeof(array)/sizeof(array[0]);
+    quicksort(array, 0, arr_length-1);
+    for (int k=0; k< arr_length; k++){
+        cout << array[k] << " ";
+    }
+
+}
